e40fd2e0b743ac0e7b8a5675505c1050dfe66346
MumaBrian <brianmuma5@gmail.com>
2022-10-31 08:17:05 -0400

    login

--- intern/urls.py
+++ intern/urls.py
@@ -20,7 +20,9 @@ from api import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.ListApi.as_view()),
-    path("api/", include("dj_rest_auth.urls")),
+    path('api-auth',include('rest_framework.urls')),
+
+    # path("api/", include("dj_rest_auth.urls")),
     # path('register',views.RegisterUserAPIView.as_view()),
 
 ]
